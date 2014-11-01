80dfcf7ef7fc25e6cde08fa0dcd9a6dee845d401
Darren Ethier <darren@roughsmootheng.in>
2014-11-01 13:59:15 -0400

    fix undefined property

--- inc/React.php
+++ inc/React.php
@@ -16,7 +16,7 @@ class React {
 	public function __construct( Request $request ) {
 		$this->_config = Config::instance();
 		$webhookReqFactory = new OutgoingWebhookRequestFactory();
-		$webhookReq = $webhookReqFactory->create( $request->get_all );
+		$webhookReq = $webhookReqFactory->create( $request->get_all() );
 		$triggerWord = $webhookRequest->getTriggerWord();
 
 		$this->_verify_token( $webhookRequest );
